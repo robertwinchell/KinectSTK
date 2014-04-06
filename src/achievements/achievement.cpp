7da20d60f5ec2bad8ea62256dbe65a00e5a0ecec
robertwinchell <robertzwinchell@gmail.com>
2014-04-06 09:55:49 -0700

    achievements

---
+++ src/achievements/achievement.cpp
@@ -0,0 +1,198 @@
+//
+//  SuperTuxKart - a fun racing game with go-kart
+//  Copyright (C) 2013-2014 Glenn De Jonghe
+//                     2014 Joerg Henrichs
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 3
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+
+#include "achievements/achievement.hpp"
+
+#include "achievements/achievement_info.hpp"
+#include "guiengine/dialog_queue.hpp"
+#include "io/utf_writer.hpp"
+#include "states_screens/dialogs/notification_dialog.hpp"
+#include "utils/log.hpp"
+#include "utils/translation.hpp"
+
+#include <stdlib.h>
+
+/** Constructur, initialises this object with the data from the
+ *  corresponding AchievementInfo.
+ */
+Achievement::Achievement(const AchievementInfo * info)
+           : m_achievement_info(info)
+{
+    m_id       = info->getID();
+    m_achieved = false;
+}   // Achievement
+
+// ----------------------------------------------------------------------------
+Achievement::~Achievement()
+{
+}   // ~Achievement
+
+// ----------------------------------------------------------------------------
+/** Loads the value from an XML node.
+ *  \param input*/
+void Achievement::load(const XMLNode *node)
+{
+    node->get("id",       &m_id      );
+    node->get("achieved", &m_achieved);
+
+    for (unsigned int i = 0; i < node->getNumNodes(); i++)
+    {
+        const XMLNode *n = node->getNode(i);
+        std::string key = n->getName();
+        int value = 0;
+        n->get("value", &value);
+        m_progress_map[key] = value;
+    }
+}   // load
+
+// ----------------------------------------------------------------------------
+/** Saves the achievement status to a file.
+ *  \param Output stream.
+ */
+void Achievement::save(UTFWriter &out)
+{
+    out << L"        <achievement id=\"" << m_id << L"\" "
+        << L"achieved=\"" << m_achieved << "\"";
+    if (isAchieved())
+    {
+        out << "/>\n";
+        return;
+    }
+
+    out << ">\n";
+    std::map<std::string, int>::iterator i;
+    for (i = m_progress_map.begin(); i != m_progress_map.end(); ++i)
+    {
+        out << "          <" << i->first
+            << " value=\"" << i->second << "\"/>\n";
+    }
+    out << "        </achievement>\n";
+}   // save
+
+// ----------------------------------------------------------------------------
+/** Returns the value for a key.
+ */
+int Achievement::getValue(const std::string & key)
+{
+    if (m_progress_map.find(key) != m_progress_map.end())
+        return m_progress_map[key];
+    return 0;
+}
+// ----------------------------------------------------------------------------
+/** Resets all currently key values to 0. Called if the reset-after-race flag
+ *  is set for the corresponding AchievementInfo.
+ */
+void Achievement::reset()
+{
+    std::map<std::string, int>::iterator iter;
+    for (iter = m_progress_map.begin(); iter != m_progress_map.end(); ++iter)
+    {
+        iter->second = 0;
+    }
+}   // reset
+
+// ----------------------------------------------------------------------------
+/** Returns how much of an achievement has been achieved in the form n/m.
+ *  The AchievementInfo adds up all goal values to get 'm', and this
+ *  this class end up all current key values for 'n'.
+ */
+irr::core::stringw Achievement::getProgressAsString()
+{
+    int progress = 0;
+    std::map<std::string, int>::const_iterator iter;
+    switch (m_achievement_info->getCheckType())
+    {
+    case AchievementInfo::AC_ALL_AT_LEAST:
+        for (iter = m_progress_map.begin(); iter != m_progress_map.end(); ++iter)
+        {
+            progress += iter->second;
+        }
+        break;
+    case AchievementInfo::AC_ONE_AT_LEAST:
+        for (iter = m_progress_map.begin(); iter != m_progress_map.end(); ++iter)
+        {
+            if(iter->second>progress) progress = iter->second;
+        }
+        break;
+    default:
+        Log::fatal("Achievement", "Missing getProgressAsString for type %d.",
+                   m_achievement_info->getCheckType());
+    }
+    return StringUtils::toWString(progress) + "/" + getInfo()->toString();
+}   // getProgressAsString
+
+// ----------------------------------------------------------------------------
+/** Increases the value of a key by a specified amount, but make sure to not
+ *  increase the value above the goal (otherwise the achievement progress
+ *  could be 12/10 (e.g. if one track is used 12 times for the Christoffel
+ *  achievement), even though the achievement is not achieved.
+ *  \param key The key whose value is increased.
+ *  \param increase Amount to add to the value of this key.
+ */
+void Achievement::increase(const std::string & key, int increase)
+{
+    std::map<std::string, int>::iterator it;
+    it = m_progress_map.find(key);
+    if (it != m_progress_map.end())
+    {
+        it->second += increase;
+        if (it->second > m_achievement_info->getGoalValue(key))
+            it->second = m_achievement_info->getGoalValue(key);
+    }
+    else
+    {
+        if (increase>m_achievement_info->getGoalValue(key))
+            increase = m_achievement_info->getGoalValue(key);
+        m_progress_map[key] = increase;
+    }
+    check();
+}   // increase
+
+// ----------------------------------------------------------------------------
+/** Called at the end of a race to potentially reset values.
+ */
+void Achievement::onRaceEnd()
+{
+    if(m_achievement_info->needsResetAfterRace())
+        reset();
+}   // onRaceEnd
+
+// ----------------------------------------------------------------------------
+/** Checks if this achievement has been achieved.
+ */
+void Achievement::check()
+{
+    if(m_achieved)
+        return;
+
+    if(m_achievement_info->checkCompletion(this))
+    {
+        //show achievement
+        core::stringw s = StringUtils::insertValues(_("Completed achievement \"%s\"."),
+                                                    m_achievement_info->getTitle());
+        GUIEngine::DialogQueue::get()->pushDialog(
+            new NotificationDialog(NotificationDialog::T_Achievements, s));
+
+        //send to server
+        Online::CurrentUser::get()->onAchieving(m_id);
+        m_achieved = true;
+    }
+}   // check
+
---
+++ src/achievements/achievement_info.hpp
@@ -0,0 +1,108 @@
+//
+//  SuperTuxKart - a fun racing game with go-kart
+//  Copyright (C) 2013-2014 Glenn De Jonghe
+//                     2014 Joerg Henrichs
+
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 3
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef HEADER_ACHIEVEMENT_INFO_HPP
+#define HEADER_ACHIEVEMENT_INFO_HPP
+
+#include "utils/types.hpp"
+
+#include "io/xml_node.hpp"
+#include "achievements/achievement.hpp"
+
+#include <irrString.h>
+#include <string>
+
+// ============================================================================
+
+class Achievement;
+
+/** This is the base class for storing the definition of an achievement, e.g. 
+ *  title, description (which is common for all achievements), but also how
+ *  to achieve this achievement.
+  * \ingroup achievements
+  */
+class AchievementInfo
+{
+public:
+    /** Some handy names for the various achievements. */
+    enum { ACHIEVE_COLUMBUS   = 1,
+           ACHIEVE_FIRST      = ACHIEVE_COLUMBUS,
+           ACHIEVE_STRIKE     = 2,
+           ACHIEVE_ARCH_ENEMY = 3,
+           ACHIEVE_LAST       = ACHIEVE_ARCH_ENEMY
+    };
+    /** Achievement check type: 
+     *  ALL_AT_LEAST: All goal values must be reached (or exceeded).
+     *  ONE_AT_LEAST: At least one current value reaches or exceedes the goal.
+     */
+    enum AchievementCheckType 
+    {
+        AC_ALL_AT_LEAST,
+        AC_ONE_AT_LEAST
+    };
+
+private:
+    /** The id of this Achievement. */
+    uint32_t           m_id;
+
+    /** The title of this achievement. */
+    irr::core::stringw m_title;
+
+    /** The description of this achievement. */
+    irr::core::stringw m_description;
+
+    /** Determines how this achievement is checked if it is successful. */
+    AchievementCheckType  m_check_type;
+
+    /** The target values needed to be reached. */
+    std::map<std::string, int> m_goal_values;
+
+    /** True if the achievement needs to be reset after each race. */
+    bool m_reset_after_race;
+
+public:
+             AchievementInfo(const XMLNode * input);
+    virtual ~AchievementInfo() {};
+
+    virtual irr::core::stringw toString() const;
+    virtual bool checkCompletion(Achievement * achievement) const;
+    int getGoalValue(const std::string &key) const;
+
+    // ------------------------------------------------------------------------
+    /** Returns the id of this achievement. */
+    uint32_t getID() const { return m_id; }
+    // ------------------------------------------------------------------------
+    /** Returns the description of this achievement. */
+    irr::core::stringw getDescription() const { return m_description; }
+    // ------------------------------------------------------------------------
+    /** Returns the title of this achievement. */
+    irr::core::stringw getTitle() const { return m_title; }
+    // ------------------------------------------------------------------------
+    bool needsResetAfterRace() const { return m_reset_after_race; }
+    // ------------------------------------------------------------------------
+    /** Returns the check type for this achievement. */
+    AchievementCheckType getCheckType() const { return m_check_type; }
+    // ------------------------------------------------------------------------
+};   // class AchievementInfo
+
+
+#endif
+
+/*EOF*/
